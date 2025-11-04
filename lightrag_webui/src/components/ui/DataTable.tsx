8482476f49e60a0002fdf370d40223434f73cc39
Kysetopher <ckyser2@gmail.com>
2025-11-03 16:53:15 -0800

    fixing merge

--- lightrag_webui/src/components/ui/DataTable.tsx
+++ lightrag_webui/src/components/ui/DataTable.tsx
@@ -22,20 +22,13 @@ interface DataTableProps<TData, TValue> {
   data: TData[]
 }
 
-<<<<<<< HEAD
 const TRUNCATE = 'block overflow-hidden text-ellipsis whitespace-nowrap'
 
-=======
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
 export default function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
   const [columnSizing, setColumnSizing] = useState<ColumnSizingState>({})
   const [columnSizingInfo, setColumnSizingInfo] = useState<ColumnSizingInfoState>({} as any)
 
   useEffect(() => {
-<<<<<<< HEAD
-=======
-    // While actively resizing, prevent accidental text selection
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
     if ((columnSizingInfo as any)?.isResizingColumn) {
       document.body.style.userSelect = 'none'
       document.body.style.cursor = 'col-resize'
@@ -52,11 +45,7 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-<<<<<<< HEAD
     columnResizeMode: 'onChange',
-=======
-    columnResizeMode: 'onChange', // use 'onEnd' if you prefer applying size on mouseup
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
     onColumnSizingChange: setColumnSizing,
     onColumnSizingInfoChange: setColumnSizingInfo,
     state: { columnSizing, columnSizingInfo }
@@ -64,10 +53,7 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
 
   return (
     <div className="overflow-auto rounded-md border">
-<<<<<<< HEAD
       {/* fixed layout keeps column widths stable while resizing */}
-=======
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
       <Table className="table-fixed">
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -85,7 +71,6 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
                       maxWidth: header.column.columnDef.maxSize ?? 1200,
                       position: 'relative'
                     }}
-<<<<<<< HEAD
                     // unify padding with body cells
                     className="px-3"
                   >
@@ -104,39 +89,16 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
                     )}
 
                     {/* Big hit area; doesn't affect layout */}
-=======
-                    className="whitespace-nowrap"
-                  >
-                    {header.isPlaceholder ? null : (
-                      <div className="pr-3">
-                        {flexRender(header.column.columnDef.header, header.getContext())}
-                      </div>
-                    )}
-
-                    {/* BIG hit area (easy to grab) */}
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
                     <div
                       onMouseDown={header.getResizeHandler()}
                       onTouchStart={header.getResizeHandler()}
                       onDoubleClick={() => header.column.resetSize()}
-<<<<<<< HEAD
                       className="absolute top-0 right-0 -mr-2 h-full w-[16px] cursor-col-resize touch-none select-none z-50"
-=======
-                      className={[
-                        // large invisible hitbox that slightly overflows right side
-                        'absolute top-0 right-0 -mr-2 h-full w-[14px]',
-                        'cursor-col-resize touch-none select-none z-50'
-                      ].join(' ')}
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
                       style={{ touchAction: 'none' }}
                       aria-label="Resize column"
                       role="separator"
                       aria-orientation="vertical"
                     >
-<<<<<<< HEAD
-=======
-                      {/* visible thin line centered in the hitbox */}
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
                       <div
                         className={[
                           'absolute top-0 bottom-0 left-1/2 -translate-x-1/2 w-px',
@@ -166,18 +128,12 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
                         minWidth: cell.column.columnDef.minSize ?? 60,
                         maxWidth: cell.column.columnDef.maxSize ?? 1200
                       }}
-<<<<<<< HEAD
                       className="px-3"
                     >
                       {/* Truncate wrapper for body cells too */}
                       <div className={TRUNCATE}>
                         {flexRender(cell.column.columnDef.cell, cell.getContext())}
                       </div>
-=======
-                      className="truncate"
-                    >
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
->>>>>>> ee334457d998ea07aca69a5c1f4838a95bfd3354
                     </TableCell>
                   )
                 })}
